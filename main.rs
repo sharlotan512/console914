94317473715c1210554e947206b4e88fc4a45ca2
sharlotan512 <ogurchik.aer@gmail.com>
2026-09-01 19:28:20 +0300

    ds

--- main.rs
+++ main.rs
@@ -1,8 +1,8 @@
 fn main() {
     let mut num = 28;
     println!("asasasasassasa {}", num);
-    num = 98;
-    println!("asasasasassasa {}", num);
+    /*num = 98;
+    println!("asasasasassasa {}", num);*/
 
-    
+
 }
